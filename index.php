745c7b5d8bfc202a4826e53a972fd28cfe4102b6
keombre <keombre8@gmail.com>
2018-11-01 23:24:01 +0100

    initial

---
+++ index.php
@@ -0,0 +1,174 @@
+<?php
+
+$opts = array('http'=>array('header' => "User-Agent:GPJP-API-agent/1.0\r\n"));
+$context = stream_context_create($opts);
+
+$html = file_get_contents("https://aplikace.skolaonline.cz/SOL/PublicWeb/gpjp/KWE014_VypisTridDenni.aspx", false, $context);
+
+//file_put_contents("./test_data", $html);
+//*/
+
+//$html = file_get_contents("./test_data");
+$html = mb_convert_encoding($html, 'HTML-ENTITIES', "UTF-8");
+
+$doc = new DOMDocument();
+libxml_use_internal_errors(true);
+$doc->loadHTML($html);
+$xpath = new DOMXPath($doc);
+
+$nlist = $xpath->query("//td[@class='KuvSuplujiciHodina']");
+
+$data = [];
+
+foreach ($nlist as $node) {
+    $tooltip = $node->getAttribute('onmouseover');
+
+    $ttp_str = explode("~", substr($tooltip, strpos($tooltip, "','")+3, -2));
+    $entry = [];
+    for ($i = 0; $i < count($ttp_str); $i += 2) {
+        $entry[$ttp_str[$i]] = $ttp_str[$i+1];
+    }
+    
+    foreach (explode(", ", $entry["Třída:"]) as $class) {
+
+        $time = substr($entry["Den (vyuč. hodina):"], strpos($entry["Den (vyuč. hodina):"], "(")+1, -1);
+
+        if (!array_key_exists($class, $data))
+            $data[$class] = [];
+
+        $new = [];
+        if (array_key_exists("Výměna za:", $entry)) {
+            $new = [
+                "time"    => $time,
+                "subject" => explode(", ", $entry["Výměna za:"])[1],
+                "type"    => "add",
+                "room"    => $entry["Učebna:"],
+                "teacher" => $entry["Učitel:"]
+            ];
+        } else if (array_key_exists("Nahrazuje hodiny:", $entry)) {
+            $info_text = explode(", ", $entry["Nahrazuje hodiny:"]);
+            if (strpos($entry["Učitel:"], "<span class=AbsZdroj>") === 0) {
+                $new = [
+                    "time"    => $time,
+                    "subject" => $info_text[1],
+                    "type"    => "remove"
+                ];
+            } else if ($info_text[count($info_text)-1] != $entry["Učebna:"]) {
+                $new = [
+                    "time"    => $time,
+                    "subject" => $info_text[1],
+                    "type"    => "room",
+                    "from"    => $entry["Učebna:"],
+                    "to"      => $info_text[count($info_text)-1]
+                ];
+            } else if (strpos($entry["Nahrazuje hodiny:"], "<span class=AbsZdroj>") !== false) {
+                $new = [
+                    "time"    => $time,
+                    "subject" => $info_text[1],
+                    "type"    => "teacher",
+                    "from"    => preg_split("/<.*?>/", $entry["Nahrazuje hodiny:"])[1],
+                    "to"      => str_replace(["<span class=AbsZdroj>", "</span>"], "", $entry["Učitel:"])
+                ];
+            }
+        }
+
+        if (count($new) == 0) {
+            array_push($data[$class], ["time" => $time, "type" => "unknown"]);
+        } else if (in_array($new, $data[$class])) {
+            continue;
+        } else
+            array_push($data[$class], $new);
+    }
+}
+
+/**
+ * data response:
+ * 
+ * class: {class name}
+ *  time: {name}
+ *  subject: {name}
+ *  type: {teacher},{room},{remove},{add}
+ *  [teacher,room]
+ *   from: {name}
+ *   to:   {name}
+ *  [removed]
+ *  [added]
+ *   room:    {room}
+ *   teacher: {name}
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Dashboard</title>
+    <link href="https://fonts.googleapis.com/css?family=Oswald:300,400" rel="stylesheet">
+    <link rel="stylesheet" href="./style.css" />
+    <script src="./script.js"></script>
+</head>
+<body>
+    <div class="footer">
+        Provozovatel: Výpočetní a informační centrum GPJP
+        <span class="right">Informace jsou aktualizovány každých 10 minut</span>
+    </div>
+    <h1 class="title">Změny v rozvrhu</h1>
+    <table class="header" cellspacing="0">
+        <tr class="header">
+            <th style="width: 12%; text-indent: 3%;">Třída</th>
+            <th style="width: 10%;">Hodina</th>
+            <th style="width: 15%;">Změna</th>
+            <th style="width: 35%;"></th>
+            <th style="width: 28%;">Původní stav</th>
+        </tr>
+    </table>
+    <table class="body" id="tab1" cellspacing="0">
+        <tr class="spacing-header">
+            <th style="width: 12%;"></th>
+            <th style="width: 10%;"></th>
+            <th style="width: 15%;"></th>
+            <th style="width: 35%;"></th>
+            <th style="width: 28%;"></th>
+        </tr>
+<?php
+foreach ($data as $class => $rows) {
+    $row_c = count($rows);
+    $first = 1;
+    foreach ($rows as $row) {
+        if ($first) {
+            echo '<tr class="border"><td class="course" rowspan="'.$row_c.'">'.$class.'</td>' . PHP_EOL;
+            $first = 0;
+        } else echo '<tr>' . PHP_EOL;
+        echo '<td class="time"><span class="hour">'.
+        $row['time'].
+        '.</span><span class="subject">'.
+        explode(" ", $row['subject'])[0].
+        '</span></td>' . PHP_EOL;
+        if ($row['type'] == 'room') {
+            echo '<td class="change label">Učebna</td>
+            <td class="change to">'.roomName($row['to']).'</td>
+            <td class="change from">'.roomName($row['from']).'</td>' . PHP_EOL;
+        } else if ($row['type'] == 'teacher') {
+            echo '<td class="change label">Supluje</td>
+            <td class="change to">'.$row['to'].'</td>
+            <td class="change from">'.$row['from'].'</td>' . PHP_EOL;
+        } else if ($row['type'] == 'add') {
+            echo '<td class="change label">Nově</td>
+            <td class="change to">'.roomName($row['room']).' - '.$row['teacher'].'</td><td></td>' . PHP_EOL;
+        } else if ($row['type'] == 'remove') {
+            echo '<td class="change label">Odpadá</td><td></td><td></td>' . PHP_EOL;
+        }
+    }
+    echo '</tr>' . PHP_EOL;
+}
+
+function roomName($room) {
+    if (substr($room, 0, 1) == "J")
+        return "Jer" . substr($room, 3);
+    return "U" . $room;
+}
+?>
+    </table>
+</body>
+</html>
---
+++ script.js
@@ -0,0 +1,28 @@
+window.onload=function () {
+    startScroll()
+    setTimeout(window.location.reload, 10 * 60 * 1000)
+}
+
+function startScroll() {
+    let tab1 = document.getElementById("tab1")
+    let tab1Height = parseInt(window.getComputedStyle(tab1).height)
+    let tab2, top
+    
+    if (tab1Height + 192 > window.innerHeight) {
+        tab2 = tab1.cloneNode(true)
+        tab2.setAttribute('id', 'tab2')
+        document.body.appendChild(tab2)
+        top = 162
+        setInterval(moveDown, 50)
+    }
+
+    function moveDown() {
+        top -= 0.5;
+        if (tab2) {
+            tab2.style.top = (top + tab1Height) + "px"
+        }
+        tab1.style.top = top + "px"
+        if (top*-1 + 162 > tab1Height)
+            top = 162
+    }
+}
